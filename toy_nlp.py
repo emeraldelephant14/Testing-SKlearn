ceb78815405401fb792e4705f3625442a53adbf8
emeraldelephant14 <aksh.nambi@gmail.com>
2026-08-18 11:42:38 -0700

    Download pandas and scikit, add new fund txt files

--- toy_nlp.py
+++ toy_nlp.py
@@ -30,7 +30,10 @@ y = labels["distressed"]
 #train logistic regression model
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42
+    X, y,
+    test_size=0.3,
+    random_state=42,
+    stratify=y
 )
 
 model = LogisticRegression()
